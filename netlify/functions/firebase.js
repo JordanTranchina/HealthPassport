250a6647d676f1775ddf2e024dca206e1a8da6fd
Jordan Tranchina <Jordan12522@gmail.com>
2021-03-06 16:50:08 -0600

    linked firebase and added static HTML to homescreen

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyB2ghQCvl9Bps-uv_7ToQTiwqzcYwiSsr0",
+  authDomain: "kiei-451-4d1c2.firebaseapp.com",
+  projectId: "kiei-451-4d1c2",
+  storageBucket: "kiei-451-4d1c2.appspot.com",
+  messagingSenderId: "1064307467074",
+  appId: "1:1064307467074:web:88407534ef9360dde81edb"
+}
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
