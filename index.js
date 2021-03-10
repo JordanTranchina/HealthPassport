952a3fc3108641ba72c179594f7c63978208a507
Your Name <rob.fabry@kellogg.northwestern.edu>
2021-03-09 23:10:36 -0600

    added logout functionality, confirmed linkage to uid

--- index.js
+++ index.js
@@ -11,54 +11,60 @@ firebase.auth().onAuthStateChanged(async function (user) {
   //   let comments = document.querySelector("#comments").value
   //   console.log(`my comments are submitted`);
   // })
-
-  document.querySelector("form").addEventListener("submit", async function (event) {
-    event.preventDefault()
-
-    let commentsText = document.querySelector('#comments').value
-
-    if (commentsText.length > 0) {
-      document.querySelector('.myComments').insertAdjacentHTML('beforeend', `
-        <div class="mx-10 my-4 px-4 py-4 max-w-screen-lg my-4">
-          ${commentsText}
-        </div>
-      `)
-      document.querySelector('#comments').value = ''
-
-      let docRef = await db.collection('mycomments').add({
-        text: commentsText
-      })
-    }
-  })
-
-  let db = firebase.firestore()
-  let querySnapshot = await db.collection('mycomments').get()
-  let comments = querySnapshot.docs
-  for (let i=0; i<comments.length; i++) {
-    let commentsId = comments[i].id 
-    let commentsData = comments[i].data()
-    let commentsText = commentsData.text
-    document.querySelector('.myComments').insertAdjacentHTML('beforeend', `
-    <div class="mx-10 my-4 px-4 py-4 max-w-screen-lg my-4">
-      ${commentsText}
-    </div>
-  `)
-  }
-  
-
   if (user) {
-    // Signed in
-    // console.log('signed in')
 
-    // Ensure the signed-in user is in the users collection
-    db.collection('users').doc(user.uid).set({
-      name: user.displayName,
-      email: user.email
+    document.querySelector('.sign-in-or-sign-out').innerHTML = `
+      <a href="#" class="sign-in-or-sign-out-button w-1/2 text-right underline px-10 py-4 text-blue-600">Log Out</a>
+    `
+    document.querySelector('.sign-in-or-sign-out-button').addEventListener('click', function(event) {
+      event.preventDefault()
+      firebase.auth().signOut()
+      document.location.href = 'index.html'
     })
 
-  } else {
-    // Signed out
-    console.log('signed out')
+    document.querySelector("form").addEventListener("submit", async function (event) {
+      event.preventDefault()
+
+      let commentsText = document.querySelector('#comments').value
+
+      if (commentsText.length > 0) {
+        document.querySelector('.myComments').insertAdjacentHTML('beforeend', `
+          <div class="mx-10 my-4 px-4 py-4 max-w-screen-lg my-4">
+            ${commentsText}
+          </div>
+        `)
+        document.querySelector('#comments').value = ''
+
+        let docRef = await db.collection('mycomments').add({
+          text: commentsText,
+          userId: user.uid
+        })
+      }
+    })
+
+    let db = firebase.firestore()
+    let querySnapshot = await db.collection('mycomments').where('userId', '==', user.uid).get()
+    let comments = querySnapshot.docs
+    for (let i=0; i<comments.length; i++) {
+      let commentsId = comments[i].id 
+      let commentsData = comments[i].data()
+      let commentsText = commentsData.text
+      document.querySelector('.myComments').insertAdjacentHTML('beforeend', `
+      <div class="mx-10 my-4 px-4 py-4 max-w-screen-lg my-4">
+        ${commentsText}
+      </div>
+    `)
+    }
+
+      // Ensure the signed-in user is in the users collection
+      db.collection('users').doc(user.uid).set({
+        name: user.displayName,
+        email: user.email
+      })
+
+    } else {
+      //need to hide the rest of this form
+      document.querySelector('form').classList.add('hidden')
 
     // Initializes FirebaseUI Auth
     let ui = new firebaseui.auth.AuthUI(firebase.auth())
