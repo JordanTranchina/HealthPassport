483bfc1bcb5b854eebaf4951f4c1b3252b3a99fe
Jordan Tranchina <Jordan12522@gmail.com>
2021-03-10 11:51:45 -0600

    pushed user values to API for family

--- family.js
+++ family.js
@@ -13,54 +13,67 @@ firebase.auth().onAuthStateChanged(async function (user) {
       event.preventDefault()
 
       // declare variables
-      let shotUsername = user.displayName
+      let username = user.displayName
       let currentUserId = firebase.auth().currentUser.uid
-      // shotname
-      let shotName = document.querySelector("#shotName").value
-      console.log(`the Shot Name is ${shotName}`);
 
-      // dateInitial
-      let dateInitial = document.querySelector("#dateInitial").value
-      console.log(`the Initial shot date is ${dateInitial}`);
+      // memberName
+      let memberName = document.querySelector("#memberName").value
+      console.log(`the family member name is ${memberName}`);
 
-      // dateBooster1
-      let dateBooster1 = document.querySelector("#dateBooster1").value
-      console.log(`the Booster 1 shot date is ${dateBooster1}`);
+      // relationship
+      let relationship = document.querySelector("#relationship").value
+      console.log(`the relationship is ${relationship}`);
 
-      // dateBooster2
-      let dateBooster2 = document.querySelector("#dateBooster2").value
-      console.log(`the Booster 2 shot date is ${dateBooster2}`);
+      // age
+      let age = document.querySelector("#age").value
+      console.log(`the age is ${age}`);
 
-      let response = await fetch("/.netlify/functions/create_shot", {
+      // healthIssue
+      let healthIssue = document.querySelector("#healthIssue").value
+      console.log(`the health issue ${healthIssue}`);
+
+      // ageWhenDiagnosed
+      let ageWhenDiagnosed = document.querySelector("#ageWhenDiagnosed").value
+      console.log(`the age when diagnosed is ${ageWhenDiagnosed}`);
+
+      // issueUnderControl
+      let issueUnderControl = document.querySelector("#issueUnderControl").value
+      console.log(`the issueUnderControl value is ${issueUnderControl}`);
+
+      let response = await fetch("/.netlify/functions/create_familyMember", {
         method: "POST",
         body: JSON.stringify({
           currentUserId: currentUserId,
-          shotUsername: shotUsername,
-          shotName: shotName,
-          dateInitial: dateInitial,
-          dateBooster1: dateBooster1,
-          dateBooster2: dateBooster2
+          username: username,
+          memberName: memberName,
+          relationship: relationship,
+          age: age,
+          healthIssue: healthIssue,
+          ageWhenDiagnosed: ageWhenDiagnosed,
+          issueUnderControl: issueUnderControl
         })
       }) //end of response
-      let vaccineCard = await response.json()
-      console.log(response);
-      console.log(vaccineCard);
-      // clear values
-      document.querySelector('#shotName').value = ''
-      document.querySelector('#dateInitial').value = ''
-      document.querySelector('#dateBooster1 ').value = ''
-      document.querySelector('#dateBooster2').value = ''
-      printVaccineCard(vaccineCard)
+      // let familyMemberCard = await response.json()
+      // console.log(response);
+      // console.log(familyMemberCard);
+      // // clear values
+      // document.querySelector('#memberName').value = ''
+      // document.querySelector('#relationship').value = ''
+      // document.querySelector('#age').value = ''
+      // document.querySelector('#healthIssue').value = ''
+      // document.querySelector('#ageWhenDiagnosed').value = ''
+      // document.querySelector('#issueUnderControl').value = ''
+      // printfamilyMemberCard(family) uncomment this when printing
 
     }) // end of form submit
 
-    let response = await fetch('/.netlify/functions/get_shots')
-    let shots = await response.json()
-    for (let i = 0; i < shots.length; i++) {
-      let vaccineCard = shots[i]
-      console.log(vaccineCard);
-      printVaccineCard(vaccineCard)
-    }
+    // let response = await fetch('/.netlify/functions/get_shots')
+    // let shots = await response.json()
+    // for (let i = 0; i < shots.length; i++) {
+    //   let vaccineCard = shots[i]
+    //   // console.log(vaccineCard);
+    //   printVaccineCard(vaccineCard)
+    // }
 
   } else {
     // Signed out
@@ -82,28 +95,28 @@ firebase.auth().onAuthStateChanged(async function (user) {
   }
 })
 
-async function printVaccineCard(vaccineCard) {
-  console.log("Succesfully called printVaccineCard");
-  let shotId = vaccineCard.id
-  document.querySelector(".container-vaccineCard").insertAdjacentHTML("beforeend",
-    `
-    <div class="vaccineCard px-4 py-4 max-w-xs rounded-xl border shadow-lg my-4 mx-10">
-      <div class="shot-name font-bold text-xl mb-2">${vaccineCard.shotName}</div>
-      <div class="shots space-y-2 border-t pt-4 border-gray-300">
-        <div class="shots-row-1 flex">
-          <p class="w-1/2 text-grey-darker text-left">Initial</p>
-          <p class="w-1/2 text-grey-darker text-right">${vaccineCard.dateInitial}</p>
-        </div>
-        <div class="shots-row-2 flex">
-          <p class="w-1/2 text-grey-darker text-left">Booster 1</p>
-          <p class="w-1/2 text-grey-darker text-right">${vaccineCard.dateBooster1}</p>
-        </div>
-        <div class="shots-row-3 flex">
-          <p class="w-1/2 text-grey-darker text-left">Booster 2</p>
-          <p class="w-1/2 text-grey-darker text-right">${vaccineCard.dateBooster2}</p>
-        </div>
-      </div>
-    </div>
-    `
-  )
-}
+// async function printVaccineCard(vaccineCard) {
+//   // console.log("Succesfully called printVaccineCard");
+//   let shotId = vaccineCard.id
+//   document.querySelector(".container-vaccineCard").insertAdjacentHTML("beforeend",
+//     `
+//     <div class="vaccineCard px-4 py-4 max-w-xs rounded-xl border shadow-lg my-4 mx-10">
+//       <div class="shot-name font-bold text-xl mb-2">${vaccineCard.shotName}</div>
+//       <div class="shots space-y-2 border-t pt-4 border-gray-300">
+//         <div class="shots-row-1 flex">
+//           <p class="w-1/2 text-grey-darker text-left">Initial</p>
+//           <p class="w-1/2 text-grey-darker text-right">${vaccineCard.dateInitial}</p>
+//         </div>
+//         <div class="shots-row-2 flex">
+//           <p class="w-1/2 text-grey-darker text-left">Booster 1</p>
+//           <p class="w-1/2 text-grey-darker text-right">${vaccineCard.dateBooster1}</p>
+//         </div>
+//         <div class="shots-row-3 flex">
+//           <p class="w-1/2 text-grey-darker text-left">Booster 2</p>
+//           <p class="w-1/2 text-grey-darker text-right">${vaccineCard.dateBooster2}</p>
+//         </div>
+//       </div>
+//     </div>
+//     `
+//   )
+// }
