740c1371b55e9421559725f563beb7f62cbec7f3
Your Name <rob.fabry@kellogg.northwestern.edu>
2021-03-09 21:45:11 -0600

    comments read/write functionality

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 firebase.auth().onAuthStateChanged(async function (user) {
 
-  let db = firebase.firestore()
-  let querySnapshot = await db.collection('shots').get()
-  console.log(querySnapshot.size)
+  // let db = firebase.firestore()
+  // let querySnapshot = await db.collection('shots').get()
+  // console.log(querySnapshot.size)
 
-  let button = document.querySelector(".button")
-  button.addEventListener("click", function (event) {
+  // let button = document.querySelector(".button")
+  // button.addEventListener("click", function (event) {
+  //   event.preventDefault()
+
+  //   let comments = document.querySelector("#comments").value
+  //   console.log(`my comments are submitted`);
+  // })
+
+  document.querySelector("form").addEventListener("submit", async function (event) {
     event.preventDefault()
 
-    let illness = document.querySelector("#illness").value
-    console.log(`the illness is ${illness}`);
+    let commentsText = document.querySelector('#comments').value
+
+    if (commentsText.length > 0) {
+      document.querySelector('.myComments').insertAdjacentHTML('beforeend', `
+        <div class="mx-10 my-4 px-4 py-4 max-w-screen-lg my-4">
+          ${commentsText}
+        </div>
+      `)
+      document.querySelector('#comments').value = ''
+
+      let docRef = await db.collection('mycomments').add({
+        text: commentsText
+      })
+    }
   })
+
+  let db = firebase.firestore()
+  let querySnapshot = await db.collection('mycomments').get()
+  let comments = querySnapshot.docs
+  for (let i=0; i<comments.length; i++) {
+    let commentsId = comments[i].id 
+    let commentsData = comments[i].data()
+    let commentsText = commentsData.text
+    document.querySelector('.myComments').insertAdjacentHTML('beforeend', `
+    <div class="mx-10 my-4 px-4 py-4 max-w-screen-lg my-4">
+      ${commentsText}
+    </div>
+  `)
+  }
+  
 
   if (user) {
     // Signed in
-    console.log('signed in')
+    // console.log('signed in')
 
     // Ensure the signed-in user is in the users collection
     db.collection('users').doc(user.uid).set({
