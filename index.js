cba997faff7f6b03c5627a3d837a5c3ee28d7b98
Jordan Tranchina <Jordan12522@gmail.com>
2021-03-12 16:14:19 -0600

    Moving items within HTML to get out of the login logout form

--- index.js
+++ index.js
@@ -2,17 +2,8 @@ firebase.auth().onAuthStateChanged(async function (user) {
 
 
   if (user) {
-
-    // let docRef = await db.collection('users').doc('name').get()
-
-    // let name = doc.data()
-
-    let name = firebase.auth().currentUser.displayName
-    console.log(name);
-
-    document.querySelector(".welcome-text").insertAdjacentHTML("beforeend", `
-      <h1 class="welcome-text text-3xl md:text-5xl p-10 text-grey-darker">Welcome Back ${name}! How do you feel today?</h1>
-    `)
+    // Signed in
+    console.log('signed in')
 
     const logout = document.querySelector('#sign-in-or-sign-out');
     logout.addEventListener('click', (event) => {
@@ -38,45 +29,14 @@ firebase.auth().onAuthStateChanged(async function (user) {
       //   document.location.href = 'index.html'
     })
 
-    // document.querySelector("form").addEventListener("submit", async function (event) {
-    //   event.preventDefault()
+    // show user's name in hello message
+    let name = firebase.auth().currentUser.displayName
+    console.log(name);
+    document.querySelector(".welcome-text").insertAdjacentHTML("beforeend", `
+      <h1 class="welcome-text text-3xl md:text-5xl p-10 text-grey-darker">Welcome Back ${name}! How do you feel today?</h1>
+    `)
 
-    //   let commentsText = document.querySelector('#comments').value
 
-    //   if (commentsText.length > 0) {
-    //     document.querySelector('.myComments').insertAdjacentHTML('beforeend', `
-    //       <div class="mx-10 my-2 px-2 py-2 max-w-screen-lg">
-    //         ${commentsText}
-    //       </div>
-    //     `)
-    //     document.querySelector('#comments').value = ''
-
-    //     let docRef = await db.collection('mycomments').add({
-    //       text: commentsText,
-    //       userId: user.uid
-    //     })
-    //   }
-    // })
-
-    // let db = firebase.firestore()
-    // let querySnapshot = await db.collection('mycomments').where('userId', '==', user.uid).get()
-    // let comments = querySnapshot.docs
-    // for (let i = 0; i < comments.length; i++) {
-    //   let commentsId = comments[i].id
-    //   let commentsData = comments[i].data()
-    //   let commentsText = commentsData.text
-    //   document.querySelector('.myComments').insertAdjacentHTML('beforeend', `
-    //   <div class="mx-2 my-2 px-2 py-2 max-w-screen-lg">
-    //     ${commentsText}
-    //   </div>
-    // `)
-    // }
-
-    // Ensure the signed-in user is in the users collection
-    // db.collection('users').doc(user.uid).set({
-    //   name: user.displayName,
-    //   email: user.email
-    // })
 
   } else {
     //need to hide the rest of this form
