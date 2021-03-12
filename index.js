2071ecb794c86138dd77f9b85e1a7040c3c08583
Jordan Tranchina <Jordan12522@gmail.com>
2021-03-12 15:40:04 -0600

    overwriting display name again

--- index.js
+++ index.js
@@ -16,29 +16,36 @@ firebase.auth().onAuthStateChanged(async function (user) {
     // let docRef = await db.collection('users').doc('name').get()
 
     // let name = doc.data()
-   
+
+    let name = firebase.auth().currentUser.displayName
+    console.log(name);
+
+    document.querySelector(".welcome-text").insertAdjacentHTML("beforeend", `
+      <h1 class="welcome-text text-3xl md:text-5xl p-10 text-grey-darker">Welcome Back ${name}! How do you feel today?</h1>
+    `)
+
     const logout = document.querySelector('#sign-in-or-sign-out');
     logout.addEventListener('click', (event) => {
       event.preventDefault();
       firebase.auth().signOut()
-        console.log("logging out!");
-        document.location.href = 'index.html'
-    //   })
-    
+      console.log("logging out!");
+      document.location.href = 'index.html'
+      //   })
+
 
       // ORIGINAL FIREBASE AUTH LOGOUT
-    // document.querySelector('.sign-out').addEventListener('click', function (event) {
-    //   event.preventDefault()
-    //   firebase.auth().signOut()
-    //   console.log("logging out!");
-    //   document.location.href = 'index.html'
-    
+      // document.querySelector('.sign-out').addEventListener('click', function (event) {
+      //   event.preventDefault()
+      //   firebase.auth().signOut()
+      //   console.log("logging out!");
+      //   document.location.href = 'index.html'
 
 
-    // document.querySelector('.sign-in-or-sign-out-button').addEventListener('click', function (event) {
-    //   event.preventDefault()
-    //   firebase.auth().signOut()
-    //   document.location.href = 'index.html'
+
+      // document.querySelector('.sign-in-or-sign-out-button').addEventListener('click', function (event) {
+      //   event.preventDefault()
+      //   firebase.auth().signOut()
+      //   document.location.href = 'index.html'
     })
 
     document.querySelector("form").addEventListener("submit", async function (event) {
